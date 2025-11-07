eeff1777f565836df1d4f0eea30ed9178dff57bc
imkhlst <khalish.21muhammad07@gmail.com>
2025-11-07 22:14:38 +0700

    Tournament scraper updated

--- scraper/tournament_scraper.py
+++ scraper/tournament_scraper.py
@@ -14,20 +14,32 @@ class TournamentScraper:
         self.tournament = tournament
         self.stage = stage
         self.tier = tier
-        self.portal = next(iter(get_url(get_soup(base_url), "portal:tournament")), None)
+        self.portal = get_url(get_soup(base_url), "portal:tournament")
         
     def scrape_tier_page(self) -> set:
         logging.info("Running scrape_tier_page method of Tournament Scraper class.")
+        processed = set()
+        queue = self.portal
+        start = 0
+        url_count = len(queue)
         try:
-            if not self.portal:
-                logging.error("URL not found.")
-                return set()
-            soup = get_soup(self.portal)
-            urls = get_url(soup, "tier")
             result = set()
-            for url in urls:
-                if any(t in url for t in self.tier):
-                    result.add(url)
+            while queue and start < url_count:
+                current_url = queue.pop()
+                if current_url in processed:
+                    continue
+                
+                soup = get_soup(current_url)
+                urls = get_url(soup, "tier")
+                for url in urls:
+                    parse = urlparse(url).path.replace("/", " ")
+                    if any(t in parse for t in self.tier):
+                        result.add(url)
+                
+                processed.add(current_url)
+                start += 1
+            
+            save_json(result, "tier")
             logging.info("scrape_tier_page method completed.")
             return result
 
@@ -35,18 +47,30 @@ class TournamentScraper:
             logging.error(f"Error occurs while executing scrape_stage_page method: {e}")
             print("Error occurs while executing scrape_stage_page method")
     
-    def scrape_tournament_page(self, url: str) -> set:
+    def scrape_tournament_page(self, url: set) -> set:
         logging.info("Running scrape_tournament_page method of Tournament Scraper class.")
+        processed = set()
+        queue = url
+        start = 0
+        url_count = len(queue)
         try:
-            if not url:
-                logging.error("URL not found.")
-                return set()
-            soup = get_soup(url)
-            urls = get_url(soup, "")
             result = set()
-            for url in urls:
-                if any(t in url.lower() for t in self.tournament):
-                    result.add(url)
+            while queue and start < url_count:
+                current_url = queue.pop()
+                if current_url in processed:
+                    continue
+                
+                soup = get_soup(current_url)
+                urls = get_url(soup, "")
+                for url in urls:
+                    parse = urlparse(url).path.replace("/", " ")
+                    if any(t in parse.lower() for t in self.tournament):
+                        result.add(url)
+                
+                processed.add(current_url)
+                start += 1
+            
+            save_json(result, "tournament")
             logging.info("scrape_tournament_page method completed.")
             return result
 
@@ -54,30 +78,43 @@ class TournamentScraper:
             logging.error(f"Error occurs while executing scrape_stage_page method: {e}")
             print("Error occurs while executing scrape_stage_page method")
     
-    def scrape_stage_page(self, url: str) -> set:
+    def scrape_stage_page(self, url: set) -> set:
         logging.info("Running scrape_stage_page method of Tournament Scraper class.")
+        processed = set()
+        queue = url
+        start = 0
+        url_count = len(queue)
         try:
-            if not url:
-                logging.error("URL not found.")
-                return set()
-            soup = get_soup(url)
-            urls = get_url(soup, "")
             result = set()
-            for url in urls:
-                if any(t in url.lower() for t in self.stage):
-                    result.add(url)
+            while queue and start < url_count:
+                current_url = queue.pop()
+                if current_url in processed:
+                    continue
+                
+                soup = get_soup(current_url)
+                urls = get_url(soup, "")
+                for url in urls:
+                    parse = urlparse(url).path.replace("/", " ")
+                    if any(t in parse.lower() for t in self.stage):
+                        if "#" in url:
+                            continue
+                        if url.startswith(current_url):
+                            result.add(url)
+                
+                processed.add(current_url)
+                start += 1
+            
+            save_json(result, "stage")
             logging.info("scrape_stage_page method completed.")
             return result
         
         except Exception as e:
             logging.error(f"Error occurs while executing scrape_stage_page method: {e}")
-            print("Error occurs while executing scrape_stage_page method")      
-            
-if __name__ == "__main__":
-    # scraper = TournamentScraper()
-    # tournament_path = []
-    # for name in TIER:
-    #     tournament = scraper.path_search(name)
-    #     filtered_tournament_path = scraper.path_filter(tournament)
-    #     tournament_path.append(filtered_tournament_path)
-    pass
+            print("Error occurs while executing scrape_stage_page method")
+    
+    def run(self):
+        logging.info("Running run method of Tournament Scraper class.")
+        tier = self.scrape_tier_page()
+        tournament = self.scrape_tournament_page(tier)
+        stage = self.scrape_stage_page(tournament)
+        logging.info("run method completed.")
