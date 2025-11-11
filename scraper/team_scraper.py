8cf5508d4ac14bef265f19fe5567d49930f3c127
imkhlst <khalish.21muhammad07@gmail.com>
2025-11-11 20:49:07 +0700

    Team scraper added.

---
+++ scraper/team_scraper.py
@@ -0,0 +1,9 @@
+from urllib.parse import urlparse
+from constants.scraper_constant import *
+from logger import logging
+from utils.scraper_utils import *
+
+
+class TeamScraper:
+    def __init__(self):
+        pass
