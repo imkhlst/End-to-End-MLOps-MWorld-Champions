52e16a92fb1eef2427f4c9bb18a014210f41944e
unknown <khalish.21muhammad07@gmail.com>
2025-11-17 08:17:33 +0700

    Player scraper updated.

--- scraper/player_scraper.py
+++ scraper/player_scraper.py
@@ -5,123 +5,109 @@ from utils.scraper_utils import *
 
 
 class PlayerScraper:
-    def __init__(self,
-                 base_url: str = BASE_URL,
-                 region: list = REGION_KEYWRORD):
-        self.region = region
-        self.player = get_url(get_item(base_url), "portal:player")
-        self.team = set()
+    def __init__(self, role: list = ROLE_KEYWORD):
+        self.role = role
     
-    def get_team(self, soup):
-        logging.info("Running get_team method of Team Scraper class.")
+    def get_player_detail(self, soup) -> list:
+        logging.info("Running get_player_detail method of Player Scraper class.")
+        result = set()
         try:
-            content = get_item(soup, ".teamcard.toggle-area")
-            logging.info(f"Found {len(content)} items for team.")
-            for box in content:
-                for center in get_item(box, "center", exact=True):
-                    # print(center)
-                    team = get_element(center, "title")
-                    if not team:
-                        team = get_text(center)
-                    # print(team_name)
-                    logging.info(f"Found text: {team}.")
-                    self.team.update(team)
-        
-        except Exception as e:
-            logging.error(f"Error occur while executing get_team method: {e}")
-            print("Error occur while executing get_team method")
+            region = get_item(soup, ".fo-nttax-infobox", exact=True)
+            cards = get_item(soup, ".teamcard.toggle-area")
+            logging.info(f"Found {len(cards)} teams.")
+            for card in cards:
     
-    def get_region(self) -> list:
-        logging.info("Running get_region method of Player Scraper Class")
-        processed = set()
-        queue = list(self.player)
-        try:
-            result = []
-            for current_url in queue:
-                logging.info(f"Scraping URL: {current_url}.")
-                if current_url in processed:
-                    logging.info(f"Already scrape URL: {current_url}")
+                team_name = None
+                team_region = None
                 
-                logging.info("Fetching soup ...")
-                soup = get_soup(current_url)
-                logging.info("Soup Fetched, extracting items ...")
-                urls = get_url(soup, "")
-                logging.info(f"Found {len(urls)} items for tier.")
-                for url in urls:
-                    if "team" in url:
-                        continue
-                    parse = urlparse(url).path.replace("_", " ").lower()
-                    if any(r.lower() in parse.replace("/", " ") for r in self.region):
-                        logging.info(f"Get URL: {url}")
-                        result.append([parse._replace("_", " "), url])
+                flag = get_item(region, "span.flag a[title]")
+                if flag:
+                    team_region = get_element(flag[0], "title")
+                    logging.info(f"Found team region: {team_region}")
                 
-                processed.add(current_url)
-            save_json(result, "player_region")
-            logging.info("get_region method completed.")
-            return result
-        
-        except Exception as e:
-            logging.error(f"Error occur while executing get_region method: {e}.")
-            print("Error occur while executing get_region method")
-    
-    def get_player(self, url):
-        logging.info("Running get_player method of Player Scraper class.")
-        processed = set()
-        queue = list(url) if isinstance(url, (list, set)) else [url]
-        try:
-            result = []
-            for current_url in queue:
-                logging.info(f"Scraping URL: {current_url}.")
-                if current_url in processed:
+                team = get_item(card, "center > a[title]", exact=True)
+                if team:
+                    team_name = get_element(team, "title")
+                    team_name = re.sub(r"\(.*?\)", "", team_name).strip()
+                    logging.info(f"Found player team: {team_name}")
+                
+                if not team_name:
                     continue
                 
-                logging.info("Fetching soup ...")
-                soup = get_soup(current_url)
-                logging.info("Soup Fetched, extracting items ...")
-                content = get_item(soup, ".wikitable.collapsible.smwtable")
-                logging.info(f"Found {len(content)} items for table.")
-                for table in content:
-                    player = get_item(table, ".name")
-                    logging.info(f"Found {len(name)} items for name.")
-                    for name in player:
-                        if any(re.search(f"bg", c, re.IGNORECASE) for c in name.find_parent("tr").get("class", [])):
+                tables = get_item(card, ".wikitable-bordered.list")
+                logging.info(f"Found {len(tables)} tables.")
+                
+                for table in tables:
+                    if int(get_element(table, "data-toggle-area-content")) > 2:
+                        continue
+                    
+                    rows = get_item(table, "tr")
+                    logging.info(f"Found {len(rows)} player.")
+                    for row in rows:
+                        role = None
+                        nationality = None
+                        player_name = None
+                        
+                        img = get_item(row, "img[title]", exact=True)
+                        if img:
+                            role = get_element(img, "title")
+                            if role and "Middle" in role:
+                                role = role.replace("Middle", "Mid Lane")
+                            
+                            if not any(r in role for r in self.role):
+                                continue
+                            
+                            logging.info(f"Found player role: {role}")
+                        
+                        flag = get_item(row, "span.flag a[title]", exact=True)
+                        if flag:
+                            nationality = get_element(flag, "title")
+                            logging.info(f"Found player nationality: {nationality}")
+                        
+                        player = get_item(row, "a[title]")
+                        if player and len(player) > 1:
+                            if "" in player:
+                                continue
+                            
+                            player_name = get_element(player[-1], "title")
+                            
+                            if player_name:
+                                player_name = re.sub(r"\(.*?\)", "", player_name).strip()
+                            
+                            if player_name and ":" in player_name:
+                                player_name = player_name.split(":", 1)[1]
+                                
+                            if player_name and "championship" in player_name.lower() or "mlbb" in player_name.lower():
+                                continue
+                            
+                            logging.info(f"Found player: {player_name}")
+                        if not player_name:
                             continue
-                        sibling = name.find_parent("td").find_next_siblings("td")[1]
-                        team = sibling.find("span")
-                        team_name = team.get("data-highlighting-class", []) if team else []
-                        logging.info(f"Found player team: {team_name}")
-                        if any(t in team_name for t in list(self.team)):
-                            player_url = name.find("a").get("href")
-                            logging.info(f"Found player URL: {player_url}")
-                            result.append(player_url)
+                        
+                        logging.info(f"Player info added: {player_name, nationality, role, team_name, team_region}")
+                        result.add((player_name, nationality, role, team_name, team_region))
             
-            logging.info("get_player method completed.")
-            return result
-        
-        except Exception as e:
-            logging.error(f"Error occur while executing get_player method: {e}.")
-            print("Error occur while executing get_player method")
-    
-    def get_player_detail(self, url):
-        logging.info("Running get_player_detail method of Player Scraper class.")
-        processed = set()
-        queue = list(url) if isinstance(url, (list, set)) else [url]
-        try:
-            return
+            logging.info("get_player_detail method completed.")            
+            return list(result)
         
         except Exception as e:
             logging.error(f"Error occur while executing get_player_detail method: {e}.")
             print("Error occur while executing get_player_detail method")
     
-    def scrape_team(self, url):
-        logging.info("Running scrape_team method of Team Scraper class.")
+    def scrape_player(self, url):
+        logging.info("Running scrape_player method of Player Scraper class.")
         processed = set()
         queue = list(url) if isinstance(url, (set, list)) else [url]
         print(f"Queue: {queue[:3]}")
         try:
-            team_roster = set()
+            all_player = set()
             for item in queue:
                 logging.info(f"Found item: {item}")
+                tier = item[0]
+                if "S-Tier" in tier or any(k in tier.lower() for k in ["invitational", "snapdragon"]):
+                    logging.info(f"Skipping tournament tier: {tier}")
+                    continue
+                
                 current_url = item[2]
                 logging.info(f"Scraping URL: {current_url}")
                 if current_url in processed:
@@ -131,19 +117,19 @@ class PlayerScraper:
                 logging.info("Fetching soup ...")
                 soup = get_soup(current_url)
                 logging.info("Soup fetched, extracting items...")
-                roster = self.get_player(soup)
-                team_roster.update(roster)
+                player = self.get_player_detail(soup)
+                all_player.update(player)
                 processed.add(current_url)
                 
             logging.info("scrape_team method completed.")
-            return list(team_roster)
+            return list(all_player)
         
         except Exception as e:
-            logging.error(f"Error occur while executing scrape_team: {e}")
-            print("Error occur while executing scrape_team")
+            logging.error(f"Error occur while executing scrape_player: {e}")
+            print("Error occur while executing scrape_player")
     
     def run(self, url):
-        logging.info("Running run method of Team Scraper class.")
-        scrape_team = self.scrape_team(url)
-        save_csv(pd.DataFrame(scrape_team), "team_roster")
+        logging.info("Running run method of Player Scraper class.")
+        scrape_player = self.scrape_player(url)
+        save_csv(pd.DataFrame(scrape_player), "player")
         logging.info("run method completed")
