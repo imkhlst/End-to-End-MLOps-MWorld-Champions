8ba5ae17d29c8aa971b582cd6e7a263f70858713
unknown <khalish.21muhammad07@gmail.com>
2026-03-15 17:16:28 +0700

    Match scraper updated.

--- scraper/match_scraper.py
+++ scraper/match_scraper.py
@@ -16,87 +16,67 @@ class MatchScraper:
         self.stage = stage
         self.tier = tier
     
-    def get_selector(self,
+    def get_bracket(self,
                      soup,
-                     selector,
-                     stage,
-                     result: list = None,
-                     depth=0) -> list:
-        logging.info("Running get_selector method of Match Scraper class.")
-        try: 
-            if result is None:
-                result = []
-            # Ambil semua elemen child sesuai selector
-            elements = soup.select_one(selector)
-            if stage == "Wildcard":
-                elements = soup.select(selector + " > div")
-            logging.info(f"Found {len(elements)} items in elements.")
-            for h in elements:
-                classes = h.get("class") or []
-                logging.info(f"Found class: {classes}.")
-                # Skip jika mengandung header / connector
-                if not re.search(r"(round-body|round-center|round-lower)", " ".join(classes)):
-                    continue
-                
-                # Bangun selector baru untuk dive in 
-                next_selector = f"div.{classes[0]}"
-                logging.info(f"Next selector: {next_selector}.")
-                
-                if stage != "Wildcard":
-                    siblings = h.find_parent("div").find_all("div", recursive=False)
-                    if len(siblings) > 1:
-                        idx = siblings.index(h) + 1
-                        next_selector = f"div:nth-child({idx})"
-                        logging.info(f"Next selector: {next_selector}.")
-                
-                depth_to_level ={
-                    0: "Grand Final",
-                    2: "Final",
-                    4: "Semi-Final",
-                    6: "Quater-Final",
-                    8: "Round 1",
-                }
-                level = depth_to_level.get(depth, f"Round {depth//2}")
-                logging.info(f"Bracket Position: {level}.")
-                
-                bracket = ""
-                if level == "Grand Final" or level == "Decider Match":
-                    bracket = level
-                else:   
-                    bracket = "Upper"
-                    if any(s in selector for s in ['div.brkts-round-body > div:nth-child(1) > div:nth-child(3)', 'div.brkts-round-body > div > div:nth-child(3)']):
-                        bracket = "lower"
-                logging.info(f"Bracket level: {bracket}.")
-                
-                new_selector = f"{selector} > {next_selector}"
-                if any(re.search(r"(center)", c) for c in classes):
-                    logging.info(f"Found brkts-round-center in {new_selector}")
-                    result.append([new_selector, bracket, level])
-                    continue
-                else:
-                    # Dive deeper secara rekursif
-                    logging.info("brkts-round-center not found. Dive in started ...")
-                    self.get_selector(soup=soup, selector=new_selector, stage=stage, result=result, depth=depth + 1)
+                     selector = None,
+                     depth=0,
+                     max_depth=15) -> list:
+        try:
+            if soup is None or depth > max_depth:
+                return None, selector
             
-            logging.info(f"get_selector method completed. Found {len(result)} items for selector.")
-            return result
+            parent = soup.find_parent("div")
 
+            if parent is None:
+                return None, selector
+            
+            parent_classes = parent.get("class") or []
+
+            sibling = parent.find_previous_sibling("div")
+
+            if not parent_classes:
+                return self.get_selector(soup=parent, selector=selector, depth=depth+1)
+            
+            if selector is None:
+                selector = f".{parent_classes[0]}"
+            else:
+                selector = f".{parent_classes[0]} > {selector}"
+            
+            if sibling:
+                sibling_classes = sibling.get("class") or []
+                if any(c.endswith("header") for c in sibling_classes):
+                    depth_to_level ={
+                        1: "Grand Final",
+                        3: "Final",
+                        5: "Semi-Final",
+                        7: "Quarter-Final",
+                        9: "Round 1",
+                    }
+                    level = depth_to_level.get(depth)
+
+                    if "upper" in sibling.get_text().lower() and depth > 1:
+                        bracket = f"Upper {level}"
+                    elif "lower" in sibling.get_text().lower() and depth > 1:
+                        bracket = f"Lower {level}"
+                    else:
+                        bracket = f"{level}"
+                    
+                    logging.info(f"Get_bracket completed. Found [{selector}] for {bracket} bracket.")
+                    return bracket
+            
+            return self.get_selector(soup=parent, selector=selector, depth=depth+1)
+            
         except Exception as e:
-            logging.error(f"Error occurs while executing get_selector method: {e}")
-            print("Error occurs while executing get_selector method")
+            logging.error(f"Error occurs while executing get_bracket method: {e}")
+            print("Error occurs while executing get_bracket method")
     
     def get_detail(self,
                    soup,
                    tier,
                    tournament,
                    stage,
-                   bracket = None,
-                   level = None) -> list:
-        logging.info("Running get_detail method of Match Scraper class.")
-        if bracket is None:
-            bracket = "Group Stage"
-        if level is None:
-            level = "Group Stage"
+                   bracket) -> list:
+        logging.info("Getting detail from get_detail method of Match Scraper class.")
         results = []
         try:
             popup = get_item(soup, ".brkts-popup", exact=True)
@@ -125,7 +105,7 @@ class MatchScraper:
             score_text = get_text(score_el).strip() if score_el else ""
 
             # cek apakah ini auto-win
-            is_autowin = any(k in score_text for k in ["W", "Win", "Winner", "FF", "DQ"])
+            is_autowin = any(k in score_text for k in ["Win", "Winner", "FF", "DQ"])
             is_empty_game = (len(games) == 0) or (len(games) == 1 and not get_text(games[0]).strip())
 
             if is_autowin or is_empty_game:
@@ -135,35 +115,43 @@ class MatchScraper:
             bans_item = get_item(popup, ".brkts-popup-mapveto__ban-round")
             all_bans = []
             for b in bans_item:
+                num = get_text(b).replace("\xa0", " ").strip()[-1]
+                idx = int(num) - 1
+                while len(all_bans) <= idx:
+                    all_bans.append([])
+                
                 ban_heroes = [get_element(a, "title") for a in get_item(b, "a")]
-                all_bans.extend(ban_heroes)
-
+                all_bans[idx] = ban_heroes
             logging.info(f"Found total {len(all_bans)} banned heroes: {all_bans}")
-
-            # Pisahkan ban untuk home dan away
-            if len(all_bans) >= 10:
-                home_bans, away_bans = all_bans[:5], all_bans[5:10]
-            else:
-                mid = len(all_bans) // 2
-                home_bans, away_bans = all_bans[:mid], all_bans[mid:]
+        
+            home_bans, away_bans = [], []
+            for i, bans in enumerate(all_bans):
+                if bans == []:
+                    home_bans.append([])
+                    away_bans.append([])
+                else:
+                    mid = len(bans) // 2
+                    home, away = bans[:mid], bans[mid:]
+                    home_bans.append(home)
+                    away_bans.append(away)
             logging.info(f"Home bans: {home_bans}, Away bans: {away_bans}")
                 
             for i, game in enumerate(games, start=1):
-                duration = get_text(game)[:5].strip() or "00:00"
+                time = get_text(game)[:5].strip()
+                duration = time if re.match(r'^\d{1,2}:\d{2}$', time) else np.nan
                 logging.info(f"Found duration game: {duration}.")
                 map_name = get_text(game)[5:].strip() or "Default"
                 logging.info(f"Found map name: {map_name}.")
 
-                icon = get_item(game, "i", exact=True)
+                icon = get_item(game, ".brkts-result-label", exact=True)
+                logging.info(f"Found icon: {icon}.")
                 classes = icon.get("class", []) if icon else []
-                status = "win" if "fa-check" in classes else "loss"
+                logging.info(f"Found classes: {classes}.")
 
                 pick_heroes = [get_element(a, "title") for a in get_item(game, "a")]
                 logging.info(f"Found {len(pick_heroes)} heroes picked. {pick_heroes}.")
                 home_picks, away_picks = pick_heroes[:5], pick_heroes[5:]
-        
-                bans_item = get_item(popup, ".brkts-popup-mapveto__ban-round")
-                logging.info(f"Found {len(bans_item)} for bans.")
+                
                 results.append({
                     "date": date,
                     "game_num": i,
@@ -171,16 +159,16 @@ class MatchScraper:
                     "away_team": away_team,
                     "home_picks": home_picks,
                     "away_picks": away_picks,
-                    "home_bans": home_bans,
-                    "away_bans": away_bans,
+                    "home_bans": home_bans[i-1] if i-1 < len(home_bans) else [],
+                    "away_bans": away_bans[i-1] if i-1 < len(away_bans) else [],
                     "duration": duration,
                     "map": map_name,
-                    "winner": home_team if status == "win" else away_team,
+                    "home_status": "win" if any(c.endswith("win") for c in classes) else "loss",
+                    "away_status": "win" if any(c.endswith("loss") for c in classes) else "loss",
                     "tier": tier,
                     "tournament": tournament,
                     "stage": stage,
-                    "bracket": bracket,
-                    "position": level
+                    "bracket": bracket
                 })
             
             logging.info(f"get_detail method completed.")    
@@ -191,7 +179,7 @@ class MatchScraper:
                 print("Error occurs while executing get_detail method")
     
     def scrape_match_detail(self, url) -> list:
-        logging.info("Running scrape_match_detail method of Match Scraper class.")
+        logging.info("Getting match detail from scrape_match_detail method of Match Scraper class.")
         processed = set()
         queue = list(url) if isinstance(url, (set, list)) else [url]
         print(f"Queue: {queue[:3]}")
@@ -208,32 +196,44 @@ class MatchScraper:
                     logging.info(f"Already scraping URL: {current_url}")
                     continue
                 
-                logging.info("Fetching soup ...")
+                logging.info("Getting soup from get_soup method from scraper_utils. Fetching soup ...")
                 soup = get_soup(current_url)
-                logging.info("Soup fetched, extracting items...")
-                is_knoutout = bool(get_item(soup, ".brkts-bracket", exact=True))
-                is_group_stage = bool(get_item(soup, ".brkts-matchlist", exact=True))
-                logging.info(f"This tournament stage has {'Knockout and Group Stage' if is_knoutout==is_group_stage==True else 'Knoutout' if is_knoutout==True else 'Group Stage'} System.")
+                logging.info("Get_soup completed. Soup fetched, extracting items...")
+                match_format = get_item(soup, "li")
+                context = []
+                for i in match_format:
+                    text = get_text(i)
+                    context.append(text)
+                is_elimination = any("elimination" in t.lower() for t in context)
+                logging.info(f"This tournament stage has {'Elimination' if is_elimination==True else 'Round Robin'} Format.")
                 
-                if is_group_stage:
-                    logging.info(f"Scraping group stage matches ...")
-                    matches = get_item(soup, selector="div.brkts-matchlist-match.brkts-match-has-details.brkts-match-popup-wrapper")
-                    logging.info(f"Found {len(matches)} items for group stage match.")
+                if is_elimination==True:
+                    matches = get_item(soup, selector="div.brkts-match")
+                    logging.info(f"Get_item completed. Found match: {len(matches)}.")
+                    if not matches:
+                        logging.info(f"Running get_item method from scraper_utils. Scraping round robin matches ...")
+                        matches = get_item(soup, selector="div.brkts-match-has-details")
+                        logging.info(f"Get_item method completed. Found {len(matches)} matches.")
+                        for match in matches:
+                            result = self.get_detail(match, tier, tournament, stage, bracket=stage)
+                            for item in result:
+                                match_detail.append(item)
+                    else:
+                        for match in matches:
+                            logging.info("Getting bracket from get_bracket method of Match Scraper class.")
+                            bracket = self.get_selector(soup=match)
+                            logging.info(f"Scraping matches ...")
+                            result = self.get_detail(match, tier, tournament, stage, bracket)
+                            for item in result:
+                                match_detail.append(item)
+                    
+                else:
+                    logging.info(f"Running get_item method from scraper_utils. Scraping round robin matches ...")
+                    matches = get_item(soup, selector="div.brkts-match-has-details")
+                    logging.info(f"Get_item method completed. Found {len(matches)} matches.")
                     for match in matches:
-                        result = self.get_detail(match, tier=tier, tournament=tournament, stage=stage)
-                        for item in result:
-                            match_detail.append(item)
-                
-                if is_knoutout:
-                    logging.info(f"Scraping knockout stage matches ...")
-                    selectors = self.get_selector(soup=soup, selector='div.brkts-bracket-wrapper > div > div.brkts-round-body', stage=stage)
-                    for item in selectors:
-                        selector = item[0]
-                        bracket = item[1]
-                        level = item[2]
-                        match = get_item(soup, selector + " > div.brkts-match-has-details", exact=True)
-                        logging.info(f"Found match: {match}.")
-                        result = self.get_detail(match, tier, tournament, stage, bracket, level)
+                        logging.info(f"Scraping matches ...")
+                        result = self.get_detail(match, tier, tournament, stage, bracket=stage)
                         for item in result:
                             match_detail.append(item)
                 
