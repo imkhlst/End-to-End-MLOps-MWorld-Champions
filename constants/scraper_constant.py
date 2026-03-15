1493557ceddca24459efa308ab72ffe930d2b9fd
unknown <khalish.21muhammad07@gmail.com>
2026-03-15 17:23:00 +0700

    Scraper constant updated.

---
+++ constants/scraper_constant.py
@@ -0,0 +1,54 @@
+BASE_URL = "https://liquipedia.net/mobilelegends/"
+
+HEADERS = {"User-Agent": ("Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/142.0"),}
+
+TOURNAMENT_KEYWORDS = [
+    "msc 2025",
+    "snapdragon_pro_series 2025",
+    "indonesia season_16",
+    "indonesia season_15",
+    "malaysia season_16",
+    "malaysia season_15",
+    "philippines season_16",
+    "philippines season_15",
+    "cambodia season_8",
+    "cambodia season_9",
+    "singapore season_9",
+    "singapore season_10",
+    "mena season_7",
+    "mena season_8",
+    "mpl latam season_3",
+    "mpl latam season_4",
+    "turkiye_championship season_5",
+    "turkiye_championship season_6",
+    "mlbb_super_league season_1",
+    "mlbb_super_league season_2",
+    "mlbb_continental_championships season_5",
+    "mlbb_continental_championships season_6",
+    "msc 2025 japan",
+    "m7_world_championship japan",
+    "msc 2025 mongolia",
+    "esn national_championship 2025",
+    "mlbb_china_masters",
+    "vietnam_mlbb_championship 2025 winter",
+    "vietnam_mlbb_championship 2025 spring",
+    "mekong season_6",
+    "mekong season 5",
+]
+
+STAGE_KEYWORD = [
+    "regular_season",
+    "playoffs",
+    "wildcard",
+    "swiss_stage",
+    "group_stage",
+    "knockout_stage",
+    "last_chance",
+    "play-ins",
+    "play-in",
+    "stage_2"
+]
+
+TOURNAMENT_TIER = ["S-Tier", "A-Tier", "B-Tier", "Qualifier_Tournaments"]
+
+ROLE_KEYWORD = ["Jungler", "Roamer", "Gold Lane", "EXP Lane", "Mid Lane"]
