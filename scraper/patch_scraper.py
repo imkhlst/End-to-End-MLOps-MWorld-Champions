39abb29d4f6681c342fc6c0eb1864e4ab2161a9d
unknown <khalish.21muhammad07@gmail.com>
2026-04-05 12:25:41 +0700

    Patch scraper updated.

--- scraper/patch_scraper.py
+++ scraper/patch_scraper.py
@@ -4,37 +4,36 @@ from utils.scraper_utils import *
 
 
 class PatchScraper:
-    def __init__(self, base_url: str = BASE_URL):
-        self.portal = get_url(get_soup(base_url), "portal:patches")
+    def __init__(self):
+        self.portal = urljoin(base=BASE_URL, url="/mobilelegends/Portal:Patches")
     
     def scrape_patch(self):
         logging.info("Running scrape_patch method of Patch Scraper class.")
         result = []
         try:
-            for current_url in self.portal:
-                logging.info("Fetching soup ...")
-                soup = get_soup(current_url)
-                logging.info("Soup Fetched, extracting items ...")
-                tables = get_item(soup, ".wikitable.collapsible")
-                logging.info(f"Found {len(tables)} tables. Select the first two tables.")
-                for table in tables[:2]:
-                    rows = get_item(table, "td a[href]")
-                    logging.info(f"Found {len(rows)} items.")
-                    for item in rows:
-                        patch = get_element(item, "title")
-                        if not patch or not "Patch" in patch:
-                            continue
-                        logging.info(f"Found {patch}.")
-                        
-                        date = item.find_parent("td").find_next_sibling("td")
-                        date = get_text(date)
-                        date = parser.parse(date)
-                        date = date.date()
-                        logging.info(f"Found release date: {date}.")
-                        result.append({
-                            "patch": patch,
-                            "release_date": date
-                        })
+            logging.info("Fetching soup ...")
+            soup = get_soup(self.portal)
+            logging.info("Soup Fetched, extracting items ...")
+            tables = get_item(soup, ".wikitable.collapsible")
+            logging.info(f"Found {len(tables)} tables. Select the first two tables.")
+            for table in tables[:2]:
+                rows = get_item(table, "td a[href]")
+                logging.info(f"Found {len(rows)} items.")
+                for item in rows:
+                    patch = get_element(item, "title")
+                    if not patch or not "Patch" in patch:
+                        continue
+                    logging.info(f"Found {patch}.")
+                    
+                    date = item.find_parent("td").find_next_sibling("td")
+                    date = get_text(date)
+                    date = parser.parse(date)
+                    date = date.date()
+                    logging.info(f"Found release date: {date}.")
+                    result.append({
+                        "patch": patch,
+                        "release_date": date
+                    })
             
             logging.info("scrape_patch method completed.")
             return result
