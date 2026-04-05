e42c26438ca97e0cac5f85d93a1080681267cc7a
unknown <khalish.21muhammad07@gmail.com>
2026-04-05 12:24:55 +0700

    Hero scraper updated.

--- scraper/hero_scraper.py
+++ scraper/hero_scraper.py
@@ -3,29 +3,28 @@ from constants.scraper_constant import *
 from utils.scraper_utils import *
 
 class HeroScraper:
-    def __init__(self, base_url: str = BASE_URL):
-        self.portal = get_url(get_soup(base_url), "portal:heroes")
+    def __init__(self):
+        self.portal = urljoin(base=BASE_URL, url="/mobilelegends/Portal:Heroes")
     
     def scrape_hero(self):
         logging.info("Running scrape_hero method of Hero Scraper class.")
         result = []
         try:
-            for current_url in self.portal:
-                logging.info("Fetching soup ...")
-                soup = get_soup(current_url)
-                logging.info("Soup Fetched, extracting items ...")
-                contents = get_item(soup, ".content3")
-                for content in contents:
-                    roles = get_item(content, ".white-text")
-                    for role in roles:
-                        role_name = get_text(role).split()[0]
-                        heroes = get_item(role, ".zoom-container")
-                        for hero in heroes:
-                            hero_name = get_text(hero)
-                            logging.info(f"Adding {hero_name} as {role_name} to result.")
-                            result.append({
-                                "hero_name": hero_name,
-                                "role_name": role_name
+            logging.info("Fetching soup ...")
+            soup = get_soup(self.portal)
+            logging.info("Soup Fetched, extracting items ...")
+            contents = get_item(soup, ".content3")
+            for content in contents:
+                roles = get_item(content, ".white-text")
+                for role in roles:
+                    role_name = get_text(role).split()[0]
+                    heroes = get_item(role, ".zoom-container")
+                    for hero in heroes:
+                        hero_name = get_text(hero)
+                        logging.info(f"Adding {hero_name} as {role_name} to result.")
+                        result.append({
+                            "hero_name": hero_name,
+                            "role_name": role_name
                             })
             logging.info("Scrape_hero method completed.")
             return list(result)
